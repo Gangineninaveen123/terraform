2474860738981079dbeaade1793f4330cdf000e1
Naveen G <gangineninaveen132@gmail.com>
2025-11-16 10:12:01 +0530

    terraform

--- count_based_loop/route53.tf
+++ count_based_loop/route53.tf
@@ -1,4 +1,4 @@
-resource "aws_route53_record" "www" {
+/* resource "aws_route53_record" "www" {
   
   zone_id = var.zone_id
   count = 4 # this is loop related, creating 4 instances
@@ -6,4 +6,4 @@ resource "aws_route53_record" "www" {
   type    = "A"
   ttl     = 1
   records = [aws_instance.roboshop[count.index].private_ip] # from output, i ll get the instance name, in zero index, i ll get that ip address
-}
+} */
